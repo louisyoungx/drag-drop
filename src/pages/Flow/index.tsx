2b0569835bc8f8b55f58c020880f181d8ea56971
louisyoungx <1462648167@qq.com>
2022-05-18 15:27:53 +0800

    feat: add side bar

--- src/pages/Flow/index.tsx
+++ src/pages/Flow/index.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import './index.css'
 import Container from '../../components/Layout/Container'
-import TopBar from './TopBar'
-import Editor from './Editor'
+import AppBar from './AppBar'
+import Canvas from './Canvas'
 
 const flow = (): React.ReactElement => {
   return (
     <Container>
-      <TopBar />
-      <Editor />
+      <AppBar>
+        <Canvas />
+      </AppBar>
     </Container>
   )
 }
